813568d0e96a050b6b606fe01177e48e53f86d21
RODRIGO ALFREDO VALDES TREJO <roroavaldes@comunidad.unam.mx>
2026-05-11 13:10:07 -0600

    se añadio al seulo del escenario pasto y agua para separar zonas , textura animada agua

--- ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -42,11 +42,10 @@ std::vector<Mesh*> meshList;
 std::vector<Shader> shaderList;
 
 Camera camera;
-
-Texture brickTexture;
-Texture dirtTexture;
-Texture plainTexture;
+Texture plain;
 Texture pisoTexture;
+Texture grassTexture;
+Texture waterTexture;
 
 // Variables para control de farolas
 int numFarolasActivas = 0; // Número de farolas actualmente encendidas
@@ -221,18 +220,16 @@ void calcAverageNormals(unsigned int* indices, unsigned int indiceCount, GLfloat
 void CreateObjects()
 {
 	unsigned int indices[] = {
-		0, 3, 1,
-		1, 3, 2,
-		2, 3, 0,
-		0, 1, 2
+		0, 2, 1,
+		1, 2, 3
 	};
 
 	GLfloat vertices[] = {
 		//	x      y      z			u	  v			nx	  ny    nz
-			-1.0f, -1.0f, -0.6f,	0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
-			0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f,
-			1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
-			0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f
+		-1.0f, 0.0f, -1.0f,		 0.0f, 0.0f,	 0.0f, -1.0f, 0.0f,
+		 1.0f, 0.0f, -1.0f,		 1.0f, 0.0f,	 0.0f, -1.0f, 0.0f,
+		-1.0f, 0.0f,  1.0f,		 0.0f, 1.0f,	 0.0f, -1.0f, 0.0f,
+		 1.0f, 0.0f,  1.0f,		 1.0f, 1.0f,	 0.0f, -1.0f, 0.0f,
 	};
 
 	unsigned int floorIndices[] = {
@@ -249,11 +246,11 @@ void CreateObjects()
 
 
 	Mesh *obj1 = new Mesh();
-	obj1->CreateMesh(vertices, indices, 32, 12);
+	obj1->CreateMesh(vertices, indices, 32, 6);
 	meshList.push_back(obj1);
 
 	Mesh *obj2 = new Mesh();
-	obj2->CreateMesh(vertices, indices, 32, 12);
+	obj2->CreateMesh(vertices, indices, 32, 6);
 	meshList.push_back(obj2);
 
 	Mesh *obj3 = new Mesh();
@@ -270,7 +267,7 @@ struct ObjetoEscena {
 std::vector<glm::vec3> posicionFarolas = {
 	glm::vec3(-80.0f, 2.5f, 156.0f),
 	glm::vec3(-80.0f, 2.5f, 112.0f),
-	glm::vec3(10.0f, 0.0f, -10.0f)
+	glm::vec3(10.0f, 0.0f, -120.0f)
 };
 
 std::vector<glm::vec3> posicionArboles = {
@@ -299,8 +296,8 @@ std::vector<glm::vec3> posicionArboles2 = {
 
 
 std::vector<ObjetoEscena> bancoPositions = {
-	{glm::vec3(-110.0f, -1.5f, 157.0f), 0.0f},
-	{glm::vec3(-110.0f, -1.5f, 113.0f), 180.0f},
+	{glm::vec3(-110.0f, -1.0f, 157.0f), 0.0f},
+	{glm::vec3(-110.0f, -1.0f, 113.0f), 180.0f},
 };
 
 //edicios iguales al molino pero con diferentes posiciones y rotaciones para dar variedad al escenario
@@ -442,15 +439,14 @@ int main()
 
 	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.5f, 0.5f);
 
-	brickTexture = Texture("Textures/brick.png");
-	brickTexture.LoadTextureA();
-	dirtTexture = Texture("Textures/dirt.png");
-	dirtTexture.LoadTextureA();
-	plainTexture = Texture("Textures/plain.png");
-	plainTexture.LoadTextureA();
+	plain = Texture("Textures/plain.png");
+	plain.LoadTextureA();
 	pisoTexture = Texture("Textures/piso.tga");
 	pisoTexture.LoadTextureA();
-
+	grassTexture = Texture("Textures/pasto.jpg");
+	grassTexture.LoadTextureA();
+	waterTexture = Texture("Textures/agua.jpg");
+	waterTexture.LoadTextureA();
 
 	//avatar ligado a la camara
 	Cuerpo = Model();
@@ -617,7 +613,7 @@ int main()
 	// Farola 3
 	pointLights[pointLightCount + 2] = PointLight(1.0f, 0.6f, 0.2f,
 		0.5f, 2.5f,
-		10.0f, 3.0f, -10.0f,
+		10.0f, 7.0f, -120.0f,
 		1.0f, 0.022f, 0.0019f);
 
 	unsigned int baseLightCount = pointLightCount;
@@ -657,6 +653,7 @@ int main()
 	glm::mat4 modelaux3(1.0);
 	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
 	glm::vec2 toffset = glm::vec2(0.0f, 0.0f);
+	glm::vec2 toffsetAgua = glm::vec2(0.0f, 0.0f);
 
 	////Loop mientras no se cierra la ventana
 	while (!mainWindow.getShouldClose())
@@ -763,7 +760,7 @@ int main()
 		model = glm::mat4(1.0);
 		modelaux = glm::mat4(1.0);
 		color = glm::vec3(1.0f, 1.0f, 1.0f);
-		toffset = glm::vec2(0.0f, 0.0f);
+		toffsetAgua = glm::vec2(0.0f, 0.0f);
 		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
 
 		model = glm::mat4(1.0);
@@ -771,11 +768,42 @@ int main()
 		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
-		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
+	//	glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
 		pisoTexture.UseTexture();
 		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		meshList[2]->RenderMesh();
 
+		// -- - RENDERIZAR PASTO-- -
+		//pasto derecho molinos iglesia
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-215.0f, -1.99f, -50.0f)); // Un poco arriba del suelo
+		model = glm::scale(model, glm::vec3(85.0f, 1.0f, 250.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		grassTexture.UseTexture();
+		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		meshList[0]->RenderMesh();
+
+		//pasto izquierdo espada en la piedra castillo mario
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(140.0f, -1.99f, 110.0f)); // Un poco arriba del suelo
+		model = glm::scale(model, glm::vec3(160.0f, 1.0f, 90.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		grassTexture.UseTexture();
+		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		meshList[0]->RenderMesh();
+
+		// --- RENDERIZAR AGUA ---
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -1.99f, 250.0f)); // Un poco abajo si quieres un estanque
+		model = glm::scale(model, glm::vec3(300.0f, 1.0f, 50.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		waterTexture.UseTexture();
+		toffsetAgua = glm::vec2(angulovaria * 0.001f,0.0f);
+		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffsetAgua));
+		Material_brillante.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		meshList[1]->RenderMesh();
+		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
+	
 
 		if (camera.getCameraMode() == 1)
 		{
